79447b7058eb899c25d03d3ffdcd6d0ae6cdfe27
lucic15 <lucicluka31@gmail.com>
2023-12-13 12:31:46 +0100

    m

--- Components/Home/home.js
+++ Components/Home/home.js
@@ -2,15 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
   const modeSwitch = document.getElementById('switch');
   const themeLink = document.getElementById('theme');
 
-  // Check the user's preference
-  if (localStorage.getItem('theme') === 'dark') {
-    themeLink.href = '../global/dark-theme.css'; // Replace with your dark theme stylesheet
-    modeSwitch.checked = true;
+  // Check if a theme is stored in localStorage
+  const storedTheme = localStorage.getItem('theme');
+  if (storedTheme) {
+    // Apply the stored theme
+    themeLink.href = `../global/${storedTheme}-theme.css`; // Adjust the path accordingly
+
+    // Update the switch state based on the stored theme
+    modeSwitch.checked = storedTheme === 'dark';
   }
-  if (localStorage.getItem('theme') === 'light') {
-    themeLink.href = '../global/light-theme.css'; // Replace with your dark theme stylesheet
-    modeSwitch.checked = false``;
-  }
+
   // Toggle theme on switch change
   modeSwitch.addEventListener('change', function () {
     if (modeSwitch.checked) {
@@ -22,4 +23,3 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
-``
